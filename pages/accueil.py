7c0769704c2048b906418af6606bf99e83fd024f
MayasSonder <mayamelouah@icloud.com>
2026-05-07 15:18:50 +0200

    Create accueil.py

---
+++ pages/accueil.py
@@ -0,0 +1,17 @@
+import pandas as pd 
+import streamlit as st 
+df = pd.read_csv('users.csv')
+
+bouttond = st.sidebar.button('Déconnexion')
+username = st.session_state['username']
+st.sidebar.write(f'Bienvenue {username}')
+
+if bouttond:
+    del st.session_state['username']
+    st.switch_page("app.py")
+
+st.sidebar.page_link("pages/accueil.py", label="Accueil")
+st.sidebar.page_link("pages/photos.py", label="Les photos de mon chat")
+
+st.title('Bienvenue sur ma page')
+st.image('https://i.pinimg.com/originals/1c/69/43/1c6943c372187ed186416b895f4eb06c.gif')
